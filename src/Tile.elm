f07cc9b83ca1453e1586c67a1578df7209ad34f2
Martin Stewart <MartinSStewart@gmail.com>
2022-10-22 13:45:18 +0200

    Add train house tile

--- src/Tile.elm
+++ src/Tile.elm
@@ -1,11 +1,10 @@
 module Tile exposing
     ( Tile(..)
+    , allTiles
     , fromChar
     , getData
     , size
-    , textureHeight
     , texturePosition
-    , textureWidth
     )
 
 import Dict exposing (Dict)
@@ -16,20 +15,7 @@ import Quantity exposing (Quantity)
 
 charToTile : Dict Char Tile
 charToTile =
-    Dict.fromList
-        [ ( 'h', House )
-        , ( 'r', RailHorizontal )
-        , ( 'R', RailVertical )
-        , ( 'q', RailBottomToRight )
-        , ( 'w', RailBottomToLeft )
-        , ( 'a', RailTopToRight )
-        , ( 's', RailTopToLeft )
-        , ( 'e', RailCrossing )
-        , ( 'Q', RailStrafeUp )
-        , ( 'A', RailStrafeDown )
-        , ( 'W', RailStrafeLeft )
-        , ( 'S', RailStrafeRight )
-        ]
+    List.map (\tile -> ( getData tile |> .char, tile )) allTiles |> Dict.fromList
 
 
 fromChar : Char -> Maybe Tile
@@ -40,16 +26,6 @@ fromChar char =
 size : ( Quantity number Pixels, Quantity number Pixels )
 size =
     ( Pixels.pixels 18, Pixels.pixels 18 )
-
-
-textureWidth : Quantity number Pixels
-textureWidth =
-    Pixels.pixels 256
-
-
-textureHeight : Quantity number Pixels
-textureHeight =
-    Pixels.pixels 512
 
 
 type Tile
@@ -65,6 +41,8 @@ type Tile
     | RailStrafeUp
     | RailStrafeLeft
     | RailStrafeRight
+    | TrainHouseRight
+    | TrainHouseLeft
 
 
 texturePosition : Tile -> { topLeft : Vec2, bottomRight : Vec2 }
@@ -82,46 +60,75 @@ texturePosition tile =
         ( w, h ) =
             data.size
     in
-    { topLeft = Math.Vector2.vec2 (x * tileW) (y * tileH)
-    , bottomRight = Math.Vector2.vec2 ((x + w) * tileW) ((y + h) * tileH)
+    { topLeft = Math.Vector2.vec2 (toFloat x * tileW) (toFloat y * tileH)
+    , bottomRight = Math.Vector2.vec2 (toFloat (x + w) * tileW) (toFloat (y + h) * tileH)
     }
 
 
-getData : Tile -> { texturePosition : ( number, number ), size : ( number, number ) }
+type alias TileData =
+    { texturePosition : ( Int, Int ), size : ( Int, Int ), char : Char }
+
+
+allTiles : List Tile
+allTiles =
+    [ House
+    , RailHorizontal
+    , RailVertical
+    , RailBottomToRight
+    , RailBottomToLeft
+    , RailTopToRight
+    , RailTopToLeft
+    , RailCrossing
+    , RailStrafeUp
+    , RailStrafeDown
+    , RailStrafeLeft
+    , RailStrafeRight
+    , TrainHouseRight
+    , TrainHouseLeft
+    ]
+
+
+getData : Tile -> TileData
 getData tile =
     case tile of
         House ->
-            { texturePosition = ( 0, 1 ), size = ( 3, 2 ) }
+            { texturePosition = ( 0, 1 ), size = ( 3, 2 ), char = 'h' }
 
         RailHorizontal ->
-            { texturePosition = ( 0, 0 ), size = ( 1, 1 ) }
+            { texturePosition = ( 0, 0 ), size = ( 1, 1 ), char = 'r' }
 
         RailVertical ->
-            { texturePosition = ( 1, 0 ), size = ( 1, 1 ) }
+            { texturePosition = ( 1, 0 ), size = ( 1, 1 ), char = 'R' }
 
         RailBottomToRight ->
-            { texturePosition = ( 3, 0 ), size = ( 4, 4 ) }
+            { texturePosition = ( 3, 0 ), size = ( 4, 4 ), char = 'q' }
 
         RailBottomToLeft ->
-            { texturePosition = ( 7, 0 ), size = ( 4, 4 ) }
+            { texturePosition = ( 7, 0 ), size = ( 4, 4 ), char = 'w' }
 
         RailTopToRight ->
-            { texturePosition = ( 3, 4 ), size = ( 4, 4 ) }
+            { texturePosition = ( 3, 4 ), size = ( 4, 4 ), char = 'a' }
 
         RailTopToLeft ->
-            { texturePosition = ( 7, 4 ), size = ( 4, 4 ) }
+            { texturePosition = ( 7, 4 ), size = ( 4, 4 ), char = 's' }
 
         RailCrossing ->
-            { texturePosition = ( 2, 0 ), size = ( 1, 1 ) }
+            { texturePosition = ( 2, 0 ), size = ( 1, 1 ), char = 'e' }
 
         RailStrafeDown ->
-            { texturePosition = ( 0, 8 ), size = ( 5, 3 ) }
+            { texturePosition = ( 0, 8 ), size = ( 5, 3 ), char = 'Q' }
 
         RailStrafeUp ->
-            { texturePosition = ( 5, 8 ), size = ( 5, 3 ) }
+            { texturePosition = ( 5, 8 ), size = ( 5, 3 ), char = 'A' }
 
         RailStrafeLeft ->
-            { texturePosition = ( 0, 11 ), size = ( 3, 5 ) }
+            { texturePosition = ( 0, 11 ), size = ( 3, 5 ), char = 'W' }
 
         RailStrafeRight ->
-            { texturePosition = ( 0, 16 ), size = ( 3, 5 ) }
+            { texturePosition = ( 0, 16 ), size = ( 3, 5 ), char = 'S' }
+
+        TrainHouseRight ->
+            { texturePosition = ( 3, 11 ), size = ( 4, 4 ), char = 't' }
+
+        TrainHouseLeft ->
+            { texturePosition = ( 7, 11 ), size = ( 4, 4 ), char = 'T' }
