450e2d8a272c024d6fe9021f53c47f20ece8b031
Martin Stewart <MartinSStewart@gmail.com>
2022-12-05 09:32:25 +0100

    Fix migration

--- src/Evergreen/Migrate/V18.elm
+++ src/Evergreen/Migrate/V18.elm
@@ -1,21 +1,21 @@
 module Evergreen.Migrate.V18 exposing (..)
 
-import Evergreen.V16.Types
+import Evergreen.V17.Types
 import Evergreen.V18.Types
 import Lamdera.Migrations exposing (ModelMigration(..), MsgMigration(..))
 
 
-backendModel : Evergreen.V16.Types.BackendModel -> ModelMigration Evergreen.V18.Types.BackendModel Evergreen.V18.Types.BackendMsg
+backendModel : Evergreen.V17.Types.BackendModel -> ModelMigration Evergreen.V18.Types.BackendModel Evergreen.V18.Types.BackendMsg
 backendModel old =
     ModelUnchanged
 
 
-frontendModel : Evergreen.V16.Types.FrontendModel -> ModelMigration Evergreen.V18.Types.FrontendModel Evergreen.V18.Types.FrontendMsg
+frontendModel : Evergreen.V17.Types.FrontendModel -> ModelMigration Evergreen.V18.Types.FrontendModel Evergreen.V18.Types.FrontendMsg
 frontendModel old =
     ModelUnchanged
 
 
-frontendMsg : Evergreen.V16.Types.FrontendMsg -> MsgMigration Evergreen.V18.Types.FrontendMsg Evergreen.V18.Types.FrontendMsg
+frontendMsg : Evergreen.V17.Types.FrontendMsg -> MsgMigration Evergreen.V18.Types.FrontendMsg Evergreen.V18.Types.FrontendMsg
 frontendMsg old =
     MsgUnchanged
 
